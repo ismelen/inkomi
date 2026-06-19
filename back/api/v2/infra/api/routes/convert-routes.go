b0707f70882c3dd529358a3107d3d638b2dd3f03
ismelen <ismael.ma34k@gmail.com>
2026-06-20 00:26:29 +0200

    rm: conver routes echo comment

--- back/api/v2/infra/api/routes/convert-routes.go
+++ back/api/v2/infra/api/routes/convert-routes.go
@@ -6,13 +6,6 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-// func SetupConvertRoutes(api *echo.Echo, handler *handlers.ConvertHandler) {
-// 	api.POST("/convert", handler.Convert)
-// 	api.GET("/status/{id}", handler.CheckStatus)
-// 	api.GET("/download/{id}", handler.Download)
-// 	api.PUT("/cancel/{id}", handler.Cancel)
-// }
-
 func SetupConvertRoutes(api *chi.Mux, handler *handlers.ConvertHandler) {
 	api.Post("/convert", Wrap(handler.Convert))
 	api.Get("/status/{id}", Wrap(handler.CheckStatus))
