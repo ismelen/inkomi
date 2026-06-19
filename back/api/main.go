cbbfeea3d5cf6ce68f0b16e1ee23a7fc00f19809
ismelen <Ismael.melendez@idvia.es>
2026-06-19 13:57:57 +0200

    chi?? +-

--- back/api/main.go
+++ back/api/main.go
@@ -7,15 +7,30 @@ import (
 	"ismelen/ermc/v2/usecases"
 	"log"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	api := echo.New()
-	api.Use(middleware.RequestLogger())
-	api.Use(middleware.CORS())
-	api.Use(middleware.BodyLimit("200M"))
+	api := chi.NewRouter()
+
+	api.Use(middleware.RequestID)
+	api.Use(middleware.Logger)
+	api.Use(middleware.Recoverer)
+	api.Use(cors.Handler(cors.Options{
+    // AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+    AllowedOrigins:   []string{"https://*", "http://*"},
+    // AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+    AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+    ExposedHeaders:   []string{"Link"},
+    AllowCredentials: false,
+    MaxAge:           300, // Maximum value not ignored by any of major browsers
+  }))
+	// api := echo.New()
+	// api.Use(middleware.RequestLogger())
+	// api.Use(middleware.CORS())
+	// api.Use(middleware.BodyLimit("200M"))
 
 	pushNotifier := notifications.FirebasePushNotifier{}
 	pushNotifier.Init()
